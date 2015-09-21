b8840b2efb7c8daa6cf6b7edd36fa8a8ceb58547
chenghw <ho.won.cheng@gmail.com>
2015-09-21 13:19:04 -0400

    badges and schedules complete

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,29 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(list)
+  batch_badge = []
+  list.each do |name|
+    batch_badge << badge_maker(name)
+  end
+  batch_badge
+end
+
+def assign_rooms(list)
+  rooms = []
+  list.each do |name|
+    rooms << "Hello, #{name}! You'll be assigned to room #{list.index(name)+1}!"
+  end
+  rooms
+end
+
+def printer(attendees)
+  batch_badge_creator(attendees).each do |badge|
+    puts badge
+  end
+
+  assign_rooms(attendees).each do |room|
+    puts room
+  end
+end
