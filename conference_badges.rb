f1c3efd3297a4cd037247dee740a1777f44e2371
limerentfox <christireid@gmail.com>
2015-10-09 23:44:37 +0200

    Badges-and-schedules

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,18 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  attendees.map {|name| badge_maker(name)}
+end
+
+def assign_rooms(attendees)
+  room_assignments =[]
+  attendees.each_with_index {|name, index| room_assignments << "Hello, #{name}! You'll be assigned to room #{index + 1}!"}
+  room_assignments
+end
+
+def printer(attendees)
+  badges_and_room_assignments = batch_badge_creator(attendees) + room_assignments
+    badges_and_room_assignments.each {|line| puts line}
+  end
