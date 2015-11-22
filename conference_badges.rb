a2148f21e3d0676777ee1f9446b5d1bf3bb5c275
Shena Yoshida <sheyoshida@gmail.com>
2015-11-22 08:55:27 -0500

    in progress

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,20 @@
 # Write your code here.
+
+def badge_maker(name = "Arel")
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+    batch_of_name = []
+    batch_of_name << (attendees)
+    batch_of_name
+end
+
+def assign_rooms(attendees)
+    room_assignments
+end
+
+def printer
+puts batch_badge_creator
+puts assign_rooms
+end
