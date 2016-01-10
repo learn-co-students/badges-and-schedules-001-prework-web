f0d094cf201ac1c72fee7cf4f71a4f8f441ebf2c
fossbender <fossbender@gmail.com>
2016-01-10 13:55:19 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,26 +3,34 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(speakers)
-  badge_array = []
-  speakers.each {|speaker|
-    badge_array << badge_maker(speaker)}
-  badge_array
+  #badge_array = []
+  #speakers.each {|speaker|
+    #badge_array << badge_maker(speaker)}
+  #badge_array
+  speakers.map {|speaker|
+    "Hello, my name is #{speaker}."  }
 end
 
 def assign_rooms(speakers)
-  speaker_rooms = []
-  speakers.each_with_index {|speaker,index |
-    speaker_rooms << "Hello, #{speaker}! You'll be assigned to room #{index+1}!"}
-  speaker_rooms
+  #speaker_rooms = []
+  #speakers.each_with_index {|speaker,index |
+  #  speaker_rooms << "Hello, #{speaker}! You'll be assigned to room #{index+1}!"}
+  #speaker_rooms
+  #speakers.each_with_index.map { |speaker,index |
+  #  "Hello, #{speaker}! You'll be assigned to room #{index+1}!"}
+  def assign_rooms(attendees)
+    new_arr = []
+    new_arr.push(attendees.each_with_index.collect {|name, index| "Hello, #{name}! You'll be assigned to room #{index+1}!"})
+    return new_arr
+  end
+
 end
 
 def printer(speakers)
-  badges = batch_badge_creator(speakers)
-  rooms = assign_rooms(speakers)
-
-  rooms.each {|room|
-    puts room}
-  
-  badges.each {|badge|
-    puts badge}
+  #badges = batch_badge_creator(speakers)
+  #rooms = assign_rooms(speakers)
+  #rooms.each {|room| puts room}
+  #badges.each {|badge| puts badge}
+  batch_badge_creator(speakers).each {|badge| puts badge}
+  assign_rooms(speakers).each {|speaker| puts speaker}
 end
--- each-with-index.rb
+++
@@ -1,4 +0,0 @@
-myarray = ["cat" ,"dog", "wombat"]
-myarray.each_with_index do |item, index|
-  puts "index: #{index} is item: #{item}"
-  end
