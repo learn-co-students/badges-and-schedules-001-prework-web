fcb6ab2fcae468f2e5b29b91947e75e9f002e784
akhinchey <akhinchey@gmail.com>
2016-01-13 01:47:47 -0500

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,20 +1,17 @@
-def badge_maker(name) # takes argument of name of attendee
+def badge_maker(name) 
   return "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(attendees) # takes argument of array of names 
+def batch_badge_creator(attendees) 
   attendees.collect do |name| 
     "#{badge_maker(name)}" 
   end 
 end
 
-def assign_rooms(attendees)  # takes argument of array of names
-  attendees.collect do |name| # iterates, collects & returns name values into new array
-    # gives variable 'name'
-    room_num = attendees.index(name) + 1 # sets new var = index of each 'name'
-    # from array colect and adds 1
+def assign_rooms(attendees)  
+  attendees.collect do |name| 
+    room_num = attendees.index(name) + 1 
     "Hello, #{name}! You'll be assigned to room #{room_num}!"
-    # above line returns each name with its array index + 1
   end
 end
 
