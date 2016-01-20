a71cefa73ab1bc3a8300ef3ce1611f907be006f0
danielperzales <dani3l122@hotmail.com>
2016-01-20 13:42:06 -0500

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,18 @@
 # Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+def batch_badge_creator(attendees)
+  return badges
+end
+def assign_rooms(attendees)
+  return room_assignments
+end
+
+def printer(attendees)
+  badges.each {|s|puts s}
+
+  room_assignments.each {|d| puts d}
+
+
+  end 
