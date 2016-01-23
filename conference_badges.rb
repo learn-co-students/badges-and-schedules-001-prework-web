2d468d2a3df340ec9edc0d8f95b7d4e7d070f6a6
dcarrano <dcarrano@gmail.com>
2016-01-23 03:57:22 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -19,8 +19,24 @@ def assign_rooms(speaker_list)
   room_assignments = []
 
   speaker_list.each_with_index do |speaker, index|
-    room_assignments.push "Hello, #{speaker}! You'll be assigned to room " + (index+1)_to.s + "!"
+    room_assignments.push "Hello, #{speaker}! You'll be assigned to room " + (index + 1).to_s + "!"
   end
 
   room_assignments
+end
+
+def printer(speaker_list)
+
+  badges_to_print = batch_badge_creator(speaker_list)
+
+  badges_to_print.each do |badge|
+    puts badge
+  end
+
+  rooms_to_print = assign_rooms(speaker_list)
+
+  rooms_to_print.each do |room|
+    puts room
+  end
+
 end
