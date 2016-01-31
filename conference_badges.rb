c8d54623b3003cb07b066629c6ee0b6345149756
gs2589 <gabriel.seidner@gmail.com>
2016-01-31 17:38:28 -0500

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,31 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+badges=[]
+attendees.each do |att|
+badges<<badge_maker(att)
+end 
+badges
+end
+
+def assign_rooms(attendees)
+  assignments=[]
+  attendees.each_with_index do |att,idx|
+    assignments<< "Hello, #{att}! You'll be assigned to room #{(idx+1).to_s}!"
+  end
+assignments
+end
+
+def printer(attendees)
+  
+  batch_badge_creator(attendees).each do |badge|
+  puts (badge)
+  end
+
+  assign_rooms(attendees).each do |ass|
+  puts (ass)
+  end
+  
+end
