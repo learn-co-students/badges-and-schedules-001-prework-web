eab3b913d48eca06fada22e748975e2a2936345d
lookOnTheBrightSide <nkosindlovu87@gmail.com>
2016-02-07 13:51:00 +0200

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,51 @@
 # Write your code here.
+names = [ 'Edsger', 'Ada', 'Charles', 'Alan', 'Grace', 'Linus','Matz']
+
+def badge_maker(name)
+	"Hello, my name is #{name}."
+end
+def batch_badge_creator(names)
+	new_names = []
+	names.each do |name|
+		new_names << "Hello, my name is #{name}."
+	end
+	new_names
+end
+
+def assign_rooms(names)
+	rooms = 7
+	i = 1
+	new_names = []
+	while i <= 7 do
+		names.each do |name|
+		new_names << "Hello, #{name}! You'll be assigned to room #{i}!"
+		i += 1
+		end
+	end
+	new_names
+end
+def printer(names)
+    array_printer = []
+    array_printer = batch_badge_creator(names)
+    array_printer.each do |item|
+     puts item
+    end
+    array_printer = []
+    array_printer = assign_rooms(names)
+    array_printer.each do |item|
+     puts item
+    end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
