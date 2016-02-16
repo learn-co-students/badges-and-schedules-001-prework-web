7a9d45aa3b94722f71fd0cb7bc2ee652a780ef58
Joan Manuel Araujo <WISDOMBTHEGAME@GMAIL.COM>
2016-02-16 19:35:13 +0000

    nitrous-backup at Tue Feb 16 19:35:13 UTC 2016

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,17 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+def batch_badge_creator(attendees)
+  attendees = [one, two, three, four, five, six, seven]
+  attendees.each { |name| puts badge_maker }
+end
+
+
+
+# Edsger, Ada, Charles, Alan, Grace, Linus, and Matz
+
+# def batch_badge_creator
+# def  assign_rooms
+# def printer
+
+#"Hello, __! You'll be assigned to room __!"
