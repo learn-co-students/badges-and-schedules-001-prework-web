0b08796aa587520e5be0b8b5e34daf77701fe46b
iashy <iashy.ruby@gmail.com>
2016-02-29 13:10:09 -0800

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,3 +1,5 @@
+
+
 def badge_maker(name)
   return "Hello, my name is #{name}."
 end
@@ -9,31 +11,25 @@ def batch_badge_creator(array)
   new_array
 end
 
-#Defines room numbers by printing the given room out when method is called
-def room_numbers(room)
-return "You'll be assigned to room #{room}!"
-end
+#info for array
 
-#Enumerates the rooms numbers and adds them to a listed array 
-
-def batch_room_numbers(array1)
-    new_array2 = []
-    array1.each {|room| new_array2 << room_numbers(room)}
-    new_array2
-end
-
-#Theoretical for how to add the badges + room numbers together according to their index num
-
-def assign_rooms(array)
-    new_array3 = []
-    batch_badge_creator(array).each_with_index(0..6) {|room| new_array3 << room_numbers(room) }
-    new_array3
+def info_for_assign_rooms(name, room)
+  return "Hello, #{name}! You'll be assigned to room #{room}!"
 end
 
 
-#Not sure I could complete this one on time without skipping it, if it allowed me to submit
-#That's why it isn't completed.
+#attendees w/room assignments
 
-def print
+
+def assign_rooms(array, array1)
+    new_array = []
+    array.each_with_index{|name, room| new_array << info_for_assign_rooms(name, room)}
+    new_array
+end
+
+
+#Theoretical of what #print would look like if I was abale to degine #assign_rooms correctly
+
+def printer(array)
   assign_rooms
 end
