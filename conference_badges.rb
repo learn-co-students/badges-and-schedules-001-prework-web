a15dd40addb69717eb1e65684dcf4110d920d072
snlamm <shmuel.lamm@gmail.com>
2016-03-03 17:50:14 -0500

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,9 +3,6 @@ def badge_maker (name)
   return "Hello, my name is #{name}."
 end
 
-
-attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
-
 def batch_badge_creator(attendees)
   attendees.collect do |i|
    "Hello, my name is #{i}."
