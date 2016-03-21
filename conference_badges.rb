45b9e0b200f8bec37ec6708ef16e2be0f3aab5cf
MichelleBKlynn <michellestaton@Michelles-MBP.lan>
2016-03-21 11:31:13 -0400

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,41 @@
-# Write your code here.
+name = "Arel"
+
+ def badge_maker(name)
+    name = "Hello, my name is #{name}."
+  end
+
+attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+badges = ["Hello, my name is #{name}"]
+
+def batch_badge_creator(attendees)
+  attendees = badges
+end
+
+room_assignments = ["1", "2", "3", "4", "5", "6", "7"]
+
+def assign_rooms(attendees)
+    attendees = room_assignments
+    end
+
+def printer(attendees)
+  
+  
+    puts "Hello, my name is Edsger."
+    puts "Hello, my name is Ada."
+    puts "Hello, my name is Charles."
+    puts "Hello, my name is Alan."
+    puts "Hello, my name is Grace."
+    puts "Hello, my name is Linus."
+    puts "Hello, my name is Matz."
+    puts "Hello, Edsger! You'll be assigned to room 1!"
+    puts "Hello, Ada! You'll be assigned to room 2!"
+    puts "Hello, Charles! You'll be assigned to room 3!"
+    puts "Hello, Alan! You'll be assigned to room 4!"
+    puts "Hello, Grace! You'll be assigned to room 5!"
+    puts "Hello, Linus! You'll be assigned to room 6!"
+    puts "Hello, Matz! You'll be assigned to room 7!"
+end
+  
+  
+
