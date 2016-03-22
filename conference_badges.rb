1ba0ef718c18b7003ea36a36ad9a66f6a103be2a
datboiez <hill.eric@gmail.com>
2016-03-22 13:42:08 -0400

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,19 @@
-# Write your code here.
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+arr_names = %w[Edsger, Ada, Charles, Alan, Grace, Linus, Matz]
+
+def batch_badge_creator(arr_names)
+  arr_names.map {|i| "Hello, my name is #{i}."}
+end
+
+def assign_rooms(arr_names)
+    arr_names.map.with_index { |e, i| "Hello, #{e}! You'll be assigned to room #{i + 1}!" }
+end
+
+def printer(arr_names)
+  arr_names.map {|x| puts "Hello, my name is #{x}."}
+   arr_names.map.with_index { |e, i| puts "Hello, #{e}! You'll be assigned to room #{i + 1}!" }
+end
