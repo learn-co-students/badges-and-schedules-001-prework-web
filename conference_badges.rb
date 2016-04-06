5ea194e812811d99642a180470f476cdfab79598
BettinaP <bettinaprophete@gmail.com>
2016-04-05 20:18:36 -0400

    badges and scheds 2

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,22 +1,25 @@
 def badge_maker(name)
- "Hello, my name is #{name}."
+  "Hello, my name is #{name}."
 end
 
 def batch_badge_creator(attendees)
-attendees.map {|attendee| "Hello, my name is #{attendee}."}
+  attendees.map do |attendee|  
+  "Hello, my name is #{attendee}."
+  end
 end
 
 def assign_rooms(attendees)
-attendees.each_with_index.map do |attendee, index|
-  "Hello, #{attendee}! You'll be assigned to room #{index+1}!"
+  attendees.each_with_index.map do |attendee, index|
+    "Hello, #{attendee}! You'll be assigned to room #{index+1}!"
 end
 end
 
+
 def printer(attendees)
-  batch_badge_creator(attendees).each do |badge|
-    puts badge
+batch_badge_creator(attendees).each do |badge|
+  puts badge 
 end
-  assign_rooms(attendees).each do |assignment|
-    puts assignment
-  end
+assign_rooms(attendees).each do |room|
+puts room 
+end
 end
