30619562fd99c27ce81ec1db0c3a916654f9b951
Kellye Greene <kellye@gust.com>
2016-05-16 17:36:59 -0400

    WIP

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,17 @@
-# Write your code here.
+def badge_maker(name)
+  puts "Hello, my name is #{name}"
+end
+
+def batch_badge_creator(name)
+  badge_list = []
+  name.each do |badges|
+    badge_maker(badges)
+  end
+  badge_list << badges
+end
+
+def assign_rooms
+end
+
+def printer
+end
