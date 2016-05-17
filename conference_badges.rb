a843b6a8fae39769275dd430d61d3e231d84a6d2
Kellye Greene <kellye@gust.com>
2016-05-17 12:24:36 -0400

    Fuck this lab

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,17 +1,27 @@
 def badge_maker(name)
-  puts "Hello, my name is #{name}"
+  badge = "Hello, my name is #{name}."
+  badge
 end
 
-def batch_badge_creator(name)
-  badge_list = []
-  name.each do |badges|
-    badge_maker(badges)
+def batch_badge_creator(attendees)
+  attendees.map { |badges| badge_maker(badges) }
+end
+
+def assign_rooms(name)
+  rooms = []
+
+  name.each.with_index(1) do |name, room_assignment|
+    rooms << room_assignments = "Hello, #{name}! You'll be assigned to room #{room_assignment}!"
   end
-  badge_list << badges
+  rooms
+
 end
 
-def assign_rooms
-end
-
-def printer
+def printer(name)
+  batch_badge_creator(name).each do |badges|
+    puts badges
+  end
+  assign_rooms(name).each do |room_assignment|
+    puts room_assignment
+  end
 end
