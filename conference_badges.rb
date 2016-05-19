9694239585ae515f1f92c60e09b16aae73cd87d2
StanHekht <hekhtstanislau@gmail.com>
2016-05-19 13:05:57 +0000

    nitrous-backup at Thu May 19 13:05:57 UTC 2016

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,7 +2,6 @@ def badge_maker(name)
   x="Hello, my name is #{name}."
 end
 
-attendees=['Edsger', 'Ada', 'Charles', 'Alan', 'Grace', 'Linus', 'Matz']
 def batch_badge_creator(attendees)
   arr_badge_mess=[]
   attendees.each {|x| arr_badge_mess << badge_maker(x)}
