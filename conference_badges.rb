ed1c5c5ad4be750e89ecf786e203a76f9a09f348
abidiaz <abigaildiaz@gmail.com>
2016-05-25 13:48:16 -0400

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,8 +4,8 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(array)
-  array.collect {|name| "Hello, my name is " + name + "."}
- 
+  array.collect do |name| "Hello, my name is " + name + "."
+ end
 end
 
 def assign_rooms(array)
