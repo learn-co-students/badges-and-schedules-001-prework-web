c6dbf079938dd6904f25133ad20687cddfa0bf56
Bibi Khadiza <limakhadiza@yahoo.com>
2016-06-01 08:48:51 -0400

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,20 @@
-# Write your code here.
+def badge_maker(name)
+   "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  badges
+end
+
+def assign_rooms(attendees)
+  room_assignments
+end
+
+def printer(array)
+  batch_badge_creator(attendees).each do |string|
+    puts string
+  end
+  assign_rooms(attendees).each do |string|
+    puts string
+end
+end
