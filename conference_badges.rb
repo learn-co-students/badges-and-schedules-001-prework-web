4164901a803286f54c0d0dda988db170ebf5469b
Schyler Hosler <schylerhosler@gmail.com>
2016-06-16 16:52:09 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,25 @@
 # Write your code here.
+
+def badge_maker(name)
+  #One attempt:
+  #badge = "Hello, my name is #{name}."
+  #return badge
+
+  #Another attempt:
+  #"Hello, my name is #{name}."
+
+  #Another attempt:
+  #return "Hello, my name is #{name}."
+
+  #Another attempt:
+  #name = "Hello, my name is #{name}."
+  #return name
+end
+
+#def batch_badge_creator(attendees)
+  #badges = []
+  #attendees.each do |attendee|
+    #return "Hello, my name is #{attendee}"
+    #badges.push(attendee)
+  #end
+#end
