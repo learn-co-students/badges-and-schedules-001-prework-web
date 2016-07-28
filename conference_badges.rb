bc4694c8382c2499f48a366ef287d414bd56eed8
dwyn-nyc <dwyn.nyc@gmail.com>
2016-07-28 01:22:40 -0400

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,29 @@
-# Write your code here.
+def badge_maker(arg)
+	return "Hello, my name is #{arg}."
+end
+
+def batch_badge_creator(arg)
+	newarray = []
+	arg.each do |n|
+		newarray.push(badge_maker(n))
+	end
+	return newarray
+end
+
+def assign_rooms(arg)
+	newarray = []
+	arg.each.with_index(1) do |a, b|
+		newarray.push("Hello, #{a}! You'll be assigned to room #{b}!")
+	end
+	return newarray
+end
+
+def printer(attendees)
+  batch_badge_creator(attendees).each do |badge|
+    puts badge
+  end
+
+  assign_rooms(attendees).each do |assignment|
+    puts assignment
+  end
+end
