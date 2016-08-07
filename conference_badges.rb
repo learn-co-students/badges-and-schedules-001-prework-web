a0efb7a0eb9273f4a85eecc02e76f52902305fc6
Mirim An <mimicatcodes@gmail.com>
2016-08-07 04:02:01 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,31 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+
+end
+
+def batch_badge_creator(speakers)
+  speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+  messages = []
+  speakers.each_index do |i|
+    messages[i] = "Hello, my name is " + "#{speakers[i]}."
+  end
+  return messages
+end
+
+def assign_rooms(speakers)
+  speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+  speakers.map.with_index do |speaker, room_number|
+   "Hello, #{speaker}! You'll be assigned to room #{room_number+1}!"
+end
+end
+
+
+def printer(speakers)
+  batch_badge_creator(speakers).each do |result|
+    puts result
+  end
+  assign_rooms(speakers).each do |result|
+    puts result
+  end
+
+end
