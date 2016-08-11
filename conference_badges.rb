367389f450d5008658ed82da361934c1111c275f
Mirim An <mimicatcodes@gmail.com>
2016-08-11 05:42:51 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,81 +1,45 @@
+# the 2nd attempt!
 def badge_maker(name)
   return "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(speakers)
-  speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
-  badge = speakers.collect do |name|
-    badge_maker(name)
+def batch_badge_creator(names_array)
+  messages = []
+  names_array.each do |name|
+    messages << badge_maker(name)
 end
-  return badge
+messages
 end
 
-def assign_rooms(speakers)
-  speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
-  room_info = speakers.map.with_index do |name, index|
+#Or, use .collect
+# def batch_badge_creator(speakers)
+#   badge = speakers.collect do |name|
+#     badge_maker(name)
+# end
+#   return badge
+# end
+
+def assign_rooms(names_array)
+  room_info = names_array.map.with_index do |name, index|
   "Hello, #{name}! You'll be assigned to room #{index+1}!"
 end
- return room_info
+room_info
 end
 
+# # Or, use .each_with_index
+# def assign_rooms(names_array)
+#   room_info = []
+#   names_array.each_with_index do |name, index|
+#     room_info << "Hello, #{name}! You'll be assigned to room #{index+1}!"
+#   end
+#   room_info
+# end
 
-def printer(speakers)
-  batch_badge_creator(speakers).each do |result|
-    puts result
-  end
-  assign_rooms(speakers).each do |result|
-    puts result
-  end
+def printer(names_array)
+  batch_badge_creator(names_array).each do |result|
+  puts result
 end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# def badge_maker(name)
-#   return "Hello, my name is #{name}."
-#
-# end
-#
-# def batch_badge_creator(speakers)
-#   speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
-#   messages = []
-#   speakers.each_index do |i|
-#     messages[i] = "Hello, my name is " + "#{speakers[i]}."
-#   end
-#   return messages
-# end
-#
-# def assign_rooms(speakers)
-#   speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
-#   speakers.map.with_index do |speaker, room_number|
-#    "Hello, #{speaker}! You'll be assigned to room #{room_number+1}!"
-# end
-# end
-#
-#
-# def printer(speakers)
-#   batch_badge_creator(speakers).each do |result|
-#     puts result
-#   end
-#   assign_rooms(speakers).each do |result|
-#     puts result
-#   end
-#
-# end
+  assign_rooms(names_array).each do |result|
+  puts result
+end
+end
